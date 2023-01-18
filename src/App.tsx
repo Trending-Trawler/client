3bf9c406e13e50dd0e16ad12d221a698d0adc526
Tom Sonntag <tom.sonntag@sap.com>
2023-01-18 21:25:28 +0100

    color adjust

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Header} from './components/Header'
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-gradient-to-b from-purple-500 to-pink-500">
+    <div className="h-screen w-screen bg-gradient-to-b from-purple-400 to-pink-400">
       <Header />
     </div>
   )
