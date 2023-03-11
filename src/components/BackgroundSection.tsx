b86859045e0c61ff8487aadb9ba2594749aed25d
Danny123456789121 <kurthdaniel@web.de>
2023-03-11 03:44:54 +0100

    add galleryView with sample videos

--- src/components/BackgroundSection.tsx
+++ src/components/BackgroundSection.tsx
@@ -4,6 +4,10 @@ function BackgroundSection() {
 
   const videos: Video[] = [
     {
+      id: 0,
+      src: "videos/video0.mp4"
+    },
+    {
       id: 1,
       src: "videos/video1.mp4"
     },
@@ -13,11 +17,19 @@ function BackgroundSection() {
     },
     {
       id: 3,
-      src: "videos/video1.mp4"
+      src: "videos/video3.mp4"
     },
     {
       id: 4,
-      src: "videos/video1.mp4"
+      src: "videos/video4.mp4"
+    },
+    {
+      id: 5,
+      src: "videos/video5.mp4"
+    },
+    {
+      id: 6,
+      src: "videos/video3.mp4"
     }
   ];
 
@@ -25,11 +37,10 @@ function BackgroundSection() {
     <div className="flex flex-row items-center text-white">
       <div className="container rounded-lg pb-10 mx-auto backdrop-filter bg-slate-50/10 backdrop-blur-sm">
         <h1 className="pb-8 pt-6 font-bold text-center text-2xl">Choose A Background</h1>
-        <div className="flex justify-center">
+        <div className="flex w-1/2 mx-auto">
           <GalleryView videos={videos} />
         </div>
       </div>
-
     </div>
   );
 }
